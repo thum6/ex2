d029c12f6030ac7c55f616d83fb988188e27cfd7
worawat kwawthong <worawat2697@gmail.com>
2019-07-13 14:19:35 +0700

    เพิ่มหน้าเทล

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const port = 3000
 app.get('/', (req, res) => res.send('worawat'))
 app.get('/about', (req, res) => res.send('หน้า about'))
 app.get('/contact', (req, res) => res.send('หน้า เกี่ยวกับ'))
-
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
